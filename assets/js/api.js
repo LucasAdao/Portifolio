6b8cd3d5b6c46345d8fc79879e3b71f673922d8e
LucasAdao <l.adao@outlook.com>
2024-04-04 11:21:09 -0300

    Update API

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/LucasAdao/Portifolio/main/data/pro';
+    const url = 'https://raw.githubusercontent.com/LucasAdao/Portifolio/main/data/profile.json';
     const response = await fetch(url);
     const profileData = await response.json();
     return profileData;
